fd700f7dbc952385090523dafc77f6b7b087a972
Júlio Wilhelm Moerschbächer <juliowm@hotmail.com>
2015-09-12 18:55:31 -0300

    Formulários
    Só criei um funcao.js separados pro formulario de entrada e o de saida
    dai alterei certinho no código, pra evitar qualquer tipo de conflito que
    pudesse dar tendo o mesmo funcao.js pros dois

---
+++ exemplo_view/assets/js/funcao_entrada.js
@@ -0,0 +1,38 @@
+(function($) {
+
+  RemoveTableRow = function(handler) {
+    var tr = $(handler).closest('tr');
+
+    tr.fadeOut(400, function(){ 
+      tr.remove(); 
+    }); 
+
+    return false;
+  };
+  
+  AddTableRow = function() {
+      
+      var newRow = $("<tr>");
+      var cols = "";
+      
+      cols += '<td><input type="text" name="Sem_ano"></td>';
+
+      cols += '<td><input type="text" name="cod_disc"></td>'; 
+      
+      cols += '<td><input type="text" name="Cred_carg"></td>'; 
+
+      cols += '<td><input type="text" name="Cod_disc_2"></td>'; 
+
+      cols += '<td><input type="text" name="Cred_carg_2"></td>'; 
+      
+      cols += '<td class="actions">';
+      cols += '<button class="btn btn-large btn-danger" onclick="RemoveTableRow(this)" type="button">Remover</button>';
+      cols += '</td>';
+      
+      newRow.append(cols);
+      
+      $("#products-table").append(newRow);
+    
+      return false;
+  };
+})(jQuery);
